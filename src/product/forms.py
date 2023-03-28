3c164a917fa2d0755a1ae31f911fea2311bd7acc
sazzadhossain881 <77628174+sazzadhossain881@users.noreply.github.com>
2023-03-28 06:35:58 +0600

    initial commit

---
+++ src/product/forms.py
@@ -0,0 +1,26 @@
+from django.views import generic
+from django.views.generic import ListView, CreateView, UpdateView
+from django.forms import forms, ModelForm, CharField, TextInput, Textarea, BooleanField, CheckboxInput
+
+from product.models import Variant, Product
+
+
+class VariantForm(ModelForm):
+    class Meta:
+        model = Variant
+        fields = '__all__'
+        widgets = {
+            'title': TextInput(attrs={'class': 'form-control'}),
+            'description': Textarea(attrs={'class': 'form-control'}),
+            'active': CheckboxInput(attrs={'class': 'form-check-input', 'id': 'active'})
+        }
+
+class ProductForm(ModelForm):
+    class Meta:
+        model = Product        
+        fields = '__all__'
+        widgets = {
+            'title': TextInput(attrs={'class': 'form-control'}),
+            'sku': TextInput(attrs={'class': 'form-control'}),
+            'description': Textarea(attrs={'class': 'form-control'}),
+        }
